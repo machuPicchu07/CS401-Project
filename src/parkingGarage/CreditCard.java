b10f0a1c6e7786438b24cfcbab2d211ce8eaa49f
Diego <Fwuervey@gmail.com>
2025-11-13 19:42:43 -0800

    credit cards do be getting generated

--- src/parkingGarage/CreditCard.java
+++ src/parkingGarage/CreditCard.java
@@ -1,5 +1,35 @@
 package parkingGarage;
 
-public class CreditCard {
+import java.util.Random;
 
+public class CreditCard {
+	private String cardNum;
+
+	public CreditCard() {
+		this.cardNum = generateCardNum();
+	}
+
+	private String generateCardNum() {
+		String str = "";
+		Random random = new Random();
+
+		// pretend to have an invalid credit card number;
+		if (random.nextInt(6) % 5 == 0) {
+			return "0000";
+		}
+
+		// else get 16 digit card number;
+		for (int i = 1; i < 17; i++) {
+			str += Integer.toString(random.nextInt(10));
+			if (i % 4 == 0) {
+				str += " ";
+			}
+		}
+
+		return str;
+	}
+
+	public String getCardNum() {
+		return cardNum;
+	}
 }
