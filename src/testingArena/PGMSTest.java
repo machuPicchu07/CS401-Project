5048ba2420bddf024e93b8a1aece6b87b488a5ca
Diego <Fwuervey@gmail.com>
2025-11-19 18:47:52 -0800

    Add base PGMS test unit; empty for now.

---
+++ src/testingArena/PGMSTest.java
@@ -0,0 +1,16 @@
+package testingArena;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import parkingGarage.PGMS;
+
+class PGMSTest {
+
+	//PGMS Has a Client Handler Perhaps we would want to run to be able to test the saving/loading
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
